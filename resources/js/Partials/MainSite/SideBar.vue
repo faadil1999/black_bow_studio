c576a6a0e81fcbb38474152ff4ee86f4b53ed7bb
Faadil Torou <faadil@lynx-business.com>
2024-10-05 18:49:26 +0200

    fix(main-site):add menu side bar

--- resources/js/Partials/MainSite/SideBar.vue
+++ resources/js/Partials/MainSite/SideBar.vue
@@ -1,24 +1,37 @@
 <template>
-    <div
+    <div>
+        <nav
         :class="{
-            'w-screen': toogleSideBar,
+            'w-1/2': toogleSideBar,
             'w-0': !toogleSideBar,
         }"
-        class="bg-black fixed opacity-25 duration-1000 h-screen"
-    >
-        <nav
-            :class="{
-                'w-1/2': toogleSideBar,
-                'w-0': !toogleSideBar,
-            }"
-            class="fixed flex-shrink-0 duration-1000 overflow-hidden lg:-right-10 h-screen lg:-top-0"
+        class="fixed flex-shrink-0 duration-1000 overflow-hidden lg:-right-10 h-screen lg:-top-0"
         >
-            <div
-                class="flex flex-col bg-theme-green-fluo h-screen !opacity-100"
-            >
-                <button class="my-5 px-5" @click="closeSideBar()">
-                    <XIcon class="text-black" />
-                </button>
+            <div class="flex flex-col bg-black h-screen">
+                <div class="flex flex-row my-5 px-5">
+                    <h3 class="text-white">Navigate to </h3>
+                    <button @click="closeSideBar()">
+                        <XIcon class="text-white" />
+                    </button>
+                </div>
+                <ul class="space-y-16 my-auto lg:mx-10">
+                <li
+                    v-for="tab in tabs"
+                    :key="tab.name"
+                    class="text-white opacity-100"
+                    :class="{
+                    'text-gray-400': tab.selected,
+                    'hover:text-theme-green-fluo': !tab.selected,
+                    }"
+                >
+                    <a :href="tab.route">
+                    <div class="flex flex-row items-center space-x-3">
+                        <component class="w-8 h-8" :is="tab.icon" />
+                        <h3>{{ tab.name }}</h3>
+                    </div>
+                    </a>
+                </li>
+                </ul>
             </div>
         </nav>
     </div>
@@ -26,28 +39,66 @@
 
 <script setup>
 import { ref, computed, watch } from "vue";
-import { XIcon } from "lucide-vue-next";
+import {
+  AtSignIcon,
+  ContactIcon,
+  HomeIcon,
+  NewspaperIcon,
+  WorkflowIcon,
+  XIcon,
+} from "lucide-vue-next";
 
 const emit = defineEmits(["closeSideBar"]);
 const props = defineProps({
-    showSideBar: {
-        type: Boolean,
-        default: false,
-    },
+  showSideBar: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const showSideBar = computed(() => props.showSideBar);
 const toogleSideBar = ref(showSideBar.value);
 
+const tabs = computed(() => {
+  const routes = [
+    {
+      name: "Home",
+      route: "",
+      icon: HomeIcon,
+      selected: false,
+    },
+    {
+      name: "About",
+      route: "",
+      icon: NewspaperIcon,
+      selected: false,
+    },
+    {
+      name: "Works",
+      route: "",
+      icon: WorkflowIcon,
+      selected: false,
+    },
+    {
+      name: "Contact",
+      route: "",
+      icon: AtSignIcon,
+      selected: false,
+    },
+  ];
+
+  return routes;
+});
+
 watch(
-    () => showSideBar.value,
-    () => {
-        toogleSideBar.value = showSideBar.value;
-    }
+  () => showSideBar.value,
+  () => {
+    toogleSideBar.value = showSideBar.value;
+  }
 );
 
 function closeSideBar() {
-    toogleSideBar.value = false;
-    emit("closeSideBar", toogleSideBar.value);
+  toogleSideBar.value = false;
+  emit("closeSideBar", toogleSideBar.value);
 }
 </script>
