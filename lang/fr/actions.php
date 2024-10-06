adf941d625dcd700ddde63abe1a98d2b7a4ae266
Faadil Torou <faadil@lynx-business.com>
2024-10-06 08:37:29 +0200

    fix(main-site):add translation stuff

---
+++ lang/fr/actions.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the translations for all actions
+    | that can be done by the user, mainly used in buttons.
+    |
+    */
+
+    'add'             => 'Ajouter',
+    'approve'         => 'Approuver',
+    'buy'             => 'Acheter',
+    'cancel'          => 'Annuler',
+    'change'          => 'Changer',
+    'continue'        => 'Continuer',
+    'create'          => 'Créer',
+    'delete'          => 'Supprimer',
+    'edit'            => 'Modifier',
+    'export'          => 'Exporter',
+    'goto_linkedin'   => 'Voir le profil LinkedIn',
+    'join_us'         => 'Nous rejoindre',
+    'login'           => 'Se connecter',
+    'logout'          => 'Se déconnecter',
+    'open_in_new_tab' => 'Ouvrir dans un nouvel onglet',
+    'publish'         => 'Publier',
+    'register'        => "S'inscrire",
+    'reject'          => "Rejeter",
+    'reply'           => 'Répondre',
+    'report'          => 'Signaler',
+    'request_access'  => "Demander un accès",
+    'reset'           => 'Réinitialiser',
+    'restore'         => 'Restaurer',
+    'resume'          => 'Reprendre',
+    'save'            => 'Enregistrer',
+    'search'          => 'Rechercher',
+    'send'            => 'Envoyer',
+    'soft_delete'     => 'Archiver',
+    'subscribe'       => 'S’abonner',
+    'view_edit'       => 'Voir / Modifier',
+    'see_more'        => 'Voir plus',
+    'see_less'        => 'Voir moins',
+];
